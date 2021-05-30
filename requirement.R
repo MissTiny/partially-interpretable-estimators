07d327162220fcd22d00c5ba659784746c1b5b8f
Miss Tiny <jingyi-yang@uiowa.edu>
2021-05-30 04:32:13 -0500

    rough readme file and requirement file finished

---
+++ requirement.R
@@ -0,0 +1,8 @@
+#Followings are the packages required by PIE ####
+#regression
+install.packages("gglasso",repos='http://lib.stat.cmu.edu/R/CRAN/')
+install.packages("MASS",repos='http://lib.stat.cmu.edu/R/CRAN/')
+install.packages("xgboost",repos='http://lib.stat.cmu.edu/R/CRAN/')
+#Classification
+install.packages("fastDummies",repos='http://lib.stat.cmu.edu/R/CRAN/')
+
